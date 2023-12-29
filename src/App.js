bb40ce1f7c8a6776870bff5b4365ffd2a533c61c
Aman kohare <amankohare@gmail.com>
2023-12-29 09:21:41 +0530

    navbar done

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { NavbarDark } from './components/Navbar';
+import Intro from './components/pages/Intro';
+import About from './components/pages/About';
+import Projects from './components/pages/Projects';
+import Experience from './components/pages/Experience';
+import Skills from './components/pages/Skills';
+import Guest from './components/pages/Guest';
+import Contact from './components/pages/Contact';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <Router>
+      <NavbarDark />
+      <Routes>
+        <Route path="/" element={<Intro />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/guest" element={<Guest />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  Navbar,
+  Typography,
+  IconButton,
+} from "@material-tailwind/react";
+import { MenuIcon } from "@heroicons/react/outline";
+
+export function NavbarDark() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  return (
+    <Navbar
+      variant="gradient"
+      className="mx-auto bg-slate-400 text-black md:max-w-screen-lg md:px-8 lg:px-12 xl:px-20 py-3 rounded-md"
+      style={{ marginTop: "1rem", backdropFilter: "blur(8px)" }}
+    >
+      <div className="flex items-center justify-between w-full">
+        {/* Replace the text with your logo */}
+        <img
+          src="path/to/your/logo.png"
+          alt="Logo"
+          className="w-8 h-8 cursor-pointer"
+        />
+
+        <div className="hidden md:flex gap-4">
+          <Link to="/about">
+            <Typography variant="subtitle2" className="cursor-pointer">
+              About
+            </Typography>
+          </Link>
+          <Link to="/projects">
+            <Typography variant="subtitle2" className="cursor-pointer">
+              Projects
+            </Typography>
+          </Link>
+          <Link to="/experience">
+            <Typography variant="subtitle2" className="cursor-pointer">
+              Experience
+            </Typography>
+          </Link>
+          <Link to="/skills">
+            <Typography variant="subtitle2" className="cursor-pointer">
+              Skills
+            </Typography>
+          </Link>
+          <Link to="/guest">
+            <Typography variant="subtitle2" className="cursor-pointer">
+              Guest
+            </Typography>
+          </Link>
+          <Link to="/contact">
+            <Typography variant="subtitle2" className="cursor-pointer">
+              Contact
+            </Typography>
+          </Link>
+        </div>
+
+        {/* Mobile Menu Button */}
+        <IconButton
+          variant="text"
+          color="white"
+          className="md:hidden"
+          onClick={toggleMenu}
+        >
+          <MenuIcon className="h-6 w-6" />
+        </IconButton>
+
+        {/* Mobile Menu Cart */}
+        {isMenuOpen && (
+          <div
+            className="fixed top-12 right-4 h-300 w-200 bg-white shadow-md p-4 z-30"
+            onClick={closeMenu}
+          >
+            <Link to="/about" className="block">
+              <Typography variant="subtitle2" className="block">
+                About
+              </Typography>
+            </Link>
+            <Link to="/projects" className="block">
+              <Typography variant="subtitle2" className="block">
+                Projects
+              </Typography>
+            </Link>
+            <Link to="/experience" className="block">
+              <Typography variant="subtitle2" className="block">
+                Experience
+              </Typography>
+            </Link>
+            <Link to="/skills" className="block">
+              <Typography variant="subtitle2" className="block">
+                Skills
+              </Typography>
+            </Link>
+            <Link to="/guest" className="block">
+              <Typography variant="subtitle2" className="block">
+                Guest
+              </Typography>
+            </Link>
+            <Link to="/contact" className="block">
+              <Typography variant="subtitle2" className="block">
+                Contact
+              </Typography>
+            </Link>
+          </div>
+        )}
+      </div>
+    </Navbar>
+  );
+}
