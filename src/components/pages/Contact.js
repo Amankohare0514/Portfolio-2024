4922b2a4145d3c930d9b64da529a5089143b69aa
Aman kohare <amankohare@gmail.com>
2024-01-01 17:12:45 +0530

    chnaged contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,79 +1,49 @@
-import React from 'react'
+import React from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
-const Contact = () => {
-  return (
-    <>
-        <h1 className='text-3xl font-bold text-center pt-6'>Contact Us</h1>
-    <h3 className='text-lg text-center pt-6 text-slate-600'>😊 I love it when you want to contact me.</h3>
-    <hr className='mt-6 pb-6' />
-    <section className="bg-transparent">
-    <div className="container  px-6 py-2 mx-auto">
-        <div className="grid grid-cols-1 gap-2 mt-2 lg:grid-cols-2">
-            <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
-                <div>
-                    <span className="inline-block p-3 text-blue-500 rounded-full bg-blue-100/80 ">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
-                        </svg>
-                    </span>
-
-                    <h2 className="mt-4 text-base font-medium text-gray-800 ">Email</h2>
-                    <p className="mt-2 text-sm text-gray-500 ">I'm always open to chat !!!</p>
-                    <p className="mt-2 text-sm text-blue-500 ">amankohare@gmail.com</p>
-                </div>
-
-                <div>
-                    <span className="inline-block p-3 text-blue-500 rounded-full bg-blue-100/80 ">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
-                        </svg>
-                    </span>
-                    
-                    <h2 className="mt-4 text-base font-medium text-gray-800 ">Location</h2>
-                    <p className="mt-2 text-sm text-gray-500 ">Dist. Khargone Madhya Pradesh</p>
-                    <p className="mt-2 text-sm text-blue-500 ">451220</p>
-                </div>
-            </div>
-
-            <div className="p-4 py-6 rounded-lg bg-gray-50  md:p-8">
-                <form>
-                    <div className="-mx-2 md:items-center md:flex">
-                        <div className="flex-1 px-2">
-                            <label className="block mb-2 text-sm text-gray-600 ">First Name</label>
-                            <input type="text" placeholder="John " className="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg
-                              focus:border-blue-400  focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+export default function Contact() {
+    return (
+        <>
+            <h1 className='text-3xl font-bold text-center pt-6'>Contact</h1>
+            <h3 className='text-lg text-center pt-6 text-slate-600'>😊 I love it when you want to contact me</h3>
+            <hr className='mt-6 mb-8' />
+            <div className="flex items-center justify-center">
+                <div className="max-w-2xl border-none rounded-lg">
+                    <p className="mb-6 text-xl text-gray-800">I'm always open to chat, so please don't hesitate to contact me!</p>
+                    <p className="mb-6 text-xl text-gray-800">
+                        Anyways, please <span className="text-blue-500">don't just say hello</span>.
+                    </p>
+                    <p className="mb-4 text-xl text-gray-800">There are a few ways you can get in touch:</p>
+                    <div className="mb-8 text-xl text-gray-900">
+                        <p className='pt-1'>
+                            Email <span className='font-bold pl-8 text-gray-500 hover:text-cyan-800'>  amankohare@gmail.com</span>
+                        </p>
+                        <p className='pt-1'>
+                            LinkedIn <span className='font-bold pl-20 text-gray-500 hover:text-red-500'><a href='https://www.linkedin.com/in/aman-kohare-3a0678235/'>amankohare0514</a></span>
+                        </p>
+                        <p className='pt-1'>
+                            Medium <span className='font-bold pl-20 text-gray-500 hover:text-lime-800'><a href='https://medium.com/@amankohare'>amankohare0514</a></span>
+                        </p>
+                        <p className='pt-1'>
+                            Ask me Anything <span className='font-bold pl-16 text-gray-500 hover:text-lime-400'><a href='https://github.com/Amankohare0514'>on GitHub</a></span>
+                        </p>
+                    </div>
+                    <div className="max-w-3xl h-[160px] p-6 border border-gray-300 rounded-lg flex items-center">
+                        <div className="flex-grow">
+                            <h2 className="text-2xl font-semibold mb-2">Aman Kohare</h2>
+                            <p className="text-gray-700 mb-4">Front end  developer</p>
+                            <button className="mb-4 border h-10 w-28 hover:text-cyan-500 hover:bg-gray-100">Resume</button>
                         </div>
-
-                        <div className="flex-1 px-2 mt-4 md:mt-0">
-                            <label className="block mb-2 text-sm text-gray-600 ">Last Name</label>
-                            <input type="text" placeholder="Doe" className="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg
-                              focus:border-blue-400  focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                        <div className="flex space-x-4">
+                        <a href='https://github.com/Amankohare0514'> <GitHubIcon className="text-gray-600" /></a>
+                         <a href='https://www.linkedin.com/in/aman-kohare-3a0678235/'><LinkedInIcon className="text-gray-600" /></a>  
+                          <a href='https://www.instagram.com/aman__0514/'><InstagramIcon className="text-gray-600" /></a> 
                         </div>
                     </div>
-
-                    <div className="mt-4">
-                        <label className="block mb-2 text-sm text-gray-600 ">Email address</label>
-                        <input type="email" placeholder="johndoe@example.com" className="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg 
-                         focus:border-blue-400  focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
-                    </div>
-
-                    <div className="w-full mt-4">
-                        <label className="block mb-2 text-sm text-gray-600 ">Message</label>
-                        <textarea className="block w-full h-32 px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg md:h-56
-                          focus:border-blue-400  focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Message"></textarea>
-                    </div>
-
-                    <button className="w-full px-6 py-3 mt-4 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                        Send message
-                    </button>
-                </form>
+                </div>
             </div>
-        </div>
-    </div>
-</section>
-    </>
-  )
+        </>
+    );
 }
-
-export default Contact
